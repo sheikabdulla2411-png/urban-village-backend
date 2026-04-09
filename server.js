83f5e3daf493b502bda5bbfcaa871699140b0da2
Sheik Abdulla. A <sheikabdulla2411@gmail.com>
2026-04-09 08:11:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://urban-village-frontend.netlify.app',
+  credentials: true
+}));
 
 // Static folder for uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
